93823cb95dd8ecde3964604f6e870b5a9f0554dd
kangjietan <kangjietan@gmail.com>
2021-03-04 21:39:59 -0800

    Margin top for content.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,13 @@ import { Layout, Hero } from "../components";
 
 import styled from "styled-components";
 
-const MainContainer = styled.main``;
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  margin-top: 100px;
+`;
 
-const Index = () => {
+const Index: React.FunctionComponent = () => {
   return (
     <Layout>
       <MainContainer>
