9c81abfaec12c63dc0eb4ccc9cc9011952873a04
kangjietan <kangjietan@gmail.com>
2021-03-05 16:53:10 -0800

    Comments.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,12 @@
+/** Animate the outlines */
 let lineAnim = (delay: number): string =>
   `line-anim 2s ease forwards ${delay}s`;
 
+/** Animate the filling of lines */
 let fillAnim = (fill: string, delayIncrement: number, length: number) =>
   `${fill} 0.5s ease forwards ${delayIncrement * (length + 2.5)}s`;
 
+/** Animate the logo. Only animate fill for letters. */
 export function animateLogoSVG(id: string, delay = 0, delayIncrement = 0) {
   const logoPaths = document.querySelectorAll(`#${id} path`);
   for (let i = 0; i < logoPaths.length; i++) {
@@ -11,18 +14,20 @@ export function animateLogoSVG(id: string, delay = 0, delayIncrement = 0) {
     path.style.strokeDasharray = `${path.getTotalLength()}px`;
     path.style.strokeDashoffset = `${path.getTotalLength()}px`;
     delay += delayIncrement;
+    /** Fill the letters and not rectangle */
     if (i > 1) {
       path.style.animation = `${lineAnim(delay)}, ${fillAnim(
         "logo-fill",
         delayIncrement,
         logoPaths.length
       )}`;
-      continue;
+    } else {
+      path.style.animation = `${lineAnim(delay)}`;
     }
-    path.style.animation = `${lineAnim(delay)}`;
   }
 }
 
+/** Animate SVG. Animation for all paths. */
 export function animateSVG(id: string, delay = 0, delayIncrement = 0) {
   const logo = document.getElementById(id)!;
   const logoPaths = document.querySelectorAll(`#${id} path`);
