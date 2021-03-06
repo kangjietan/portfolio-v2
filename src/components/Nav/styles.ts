eb40bc9027ecb5ff74f6ee3e4e18838101204c68
kangjietan <kangjietan@gmail.com>
2021-03-05 22:03:14 -0800

    Navigation links and styling.

--- src/components/Nav/styles.ts
+++ src/components/Nav/styles.ts
@@ -8,13 +8,40 @@ export const Header = styled.header`
   height: 100px;
   background-color: var(--bg-color);
   box-shadow: 0px 10px 10px 1px var(--on-primary-color);
+  padding: 0px 50px;
 `;
 
 export const Navigation = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   position: relative;
   width: 100%;
   color: var(--on-bg);
   z-index: 11;
+  height: 100px;
 `;
 
-export const LogoContainer = styled.div``;
+export const LogoContainer = styled.div`
+  cursor: pointer;
+`;
+
+export const NavigationLinkContainer = styled.ul`
+  display: flex;
+  margin-right: 5px;
+`;
+
+export const NavigationLink = styled.li`
+  margin: 0 25px;
+  font-size: 1.5rem;
+  list-style: none;
+
+  a {
+    text-decoration: none;
+    color: white;
+  }
+
+  a:hover {
+    color: var(--secondary-color);
+  }
+`;
