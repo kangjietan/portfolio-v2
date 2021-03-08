d42d9a6d3d60e822e78b84b78762bc7f59689357
kangjietan <kangjietan@gmail.com>
2021-03-07 20:52:27 -0800

    Add Menu component to index.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Layout } from "./Layout/Layout";
 export { default as Hero } from "./Hero/Hero";
 export { default as Nav } from "./Nav/Nav";
+export { default as Menu } from "./Menu/Menu";
