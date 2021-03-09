dc63824302714f6bd93d7f4119662e4397006280
kangjietan <kangjietan@gmail.com>
2021-03-08 20:51:41 -0800

    Use typed.js for description. Rearrange svg next to greeting.

--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components';
 
 export const HeroSection = styled.section`
-  height: 150vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+export const GreetingContainer = styled.div`
+  display: flex;
+  margin-top: 2rem;
+  align-items: flex-end;
+`;
+
+export const Greeting = styled.h2`
+  color: var(--primary-variant-color);
+  font-size: 6rem;
+  margin: 0;
+  margin-right: 2rem;
+`;
+
+export const NameContainer = styled.div``;
+
+export const GreetingDescription = styled.p`
+  color: #03DAC6;
+  font-size: 4rem;
 `;
