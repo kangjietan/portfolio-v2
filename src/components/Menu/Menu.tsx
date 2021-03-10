3cec4c14f4c1f5ac41befd2acd98756882ab6bd2
kangjietan <kangjietan@gmail.com>
2021-03-09 18:34:51 -0800

    Close menu on resize. Toggle overflow y for body on menu state.

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   HamburgerButton,
   HamburgerContainer,
   ListContainer,
   MenuContainer,
   NavigationLinkContainer,
-  NavigationLink
+  NavigationLink,
 } from "./styles";
 
 const NavLinks = [
@@ -18,17 +18,34 @@ const NavLinks = [
 const Menu: React.FunctionComponent = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [hover, setHover] = useState(false);
+
+  const onResize = (e: Event) => {
+    const target = e.currentTarget as Window;
+    if (target.innerWidth > 768) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  document.documentElement.style.overflowY = menuOpen ? 'hidden' : 'visible';
+
   return (
     <MenuContainer>
       <ListContainer open={menuOpen}>
         <NavigationLinkContainer>
-        {NavLinks.map(({ section, name }) => {
-              return (
-                <NavigationLink key={section}>
-                  <a href={section}>{name}</a>
-                </NavigationLink>
-              );
-            })}
+          {NavLinks.map(({ section, name }) => {
+            return (
+              <NavigationLink key={section}>
+                <a href={section}>{name}</a>
+              </NavigationLink>
+            );
+          })}
         </NavigationLinkContainer>
       </ListContainer>
       <HamburgerContainer
