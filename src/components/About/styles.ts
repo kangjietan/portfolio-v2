04f7ff0f72a7f8e29735c2f38e811f76fc92c074
kangjietan <kangjietan@gmail.com>
2021-03-26 21:01:41 -0700

    Profile picture and description plus styling.

---
+++ src/components/About/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const AboutSection = styled.section`
+  display: flex;
+  padding: 2rem;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  max-width: 1024px;
+`;
+
+export const ImgContainer = styled.div`
+  width: 25rem;
+  margin-right: 2rem;
+`;
+
+export const DescriptionContainer = styled.div`
+  width: 75%;
+`;
+
+export const Description = styled.p`
+  font-size: 1.5rem;
+`;
