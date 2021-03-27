8eef8a2627e8762563fb9838a04116846f1b43fc
kangjietan <kangjietan@gmail.com>
2021-03-26 21:03:28 -0700

    Render About component. Add media queries to reduce padding.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout, Hero } from "../components";
+import { Layout, Hero, About } from "../components";
 
 import styled from "styled-components";
 
@@ -7,6 +7,15 @@ const MainContainer = styled.main`
   display: flex;
   flex-direction: column;
   margin-top: 100px;
+  padding: 0px 150px;
+
+  @media (max-width: 768px) {
+    padding: 0px 100px;
+  }
+
+  @media (max-width: 700px) {
+    padding: 0px 50px;
+  }
 `;
 
 const Index: React.FunctionComponent = () => {
@@ -14,6 +23,7 @@ const Index: React.FunctionComponent = () => {
     <Layout>
       <MainContainer>
         <Hero />
+        <About />
       </MainContainer>
     </Layout>
   );
