e276c394bc9216d0d2a41e29e1cab1e2ac0a564d
kangjietan <kangjietan@gmail.com>
2021-03-26 20:57:52 -0700

    Add smooth scroll to gatsby links,

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import GlobalStyle from "../../themes/GlobalStyle";
 import { Nav } from "../index";
 import { Container } from "./styles";
+import * as SmoothScroll from "smooth-scroll";
+
+/**
+ * ttps://medium.com/@chrisfitkin/how-to-smooth-scroll-links-in-gatsby-3dc445299558
+ */
+if (typeof window !== "undefined") {
+  new SmoothScroll('a[href*="#"]');
+}
 
 interface Props {
   children?: any;
