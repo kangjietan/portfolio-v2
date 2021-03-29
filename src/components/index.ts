a0fbc1d163f2fd8d7005200fc1d1728d1753aace
kangjietan <kangjietan@gmail.com>
2021-03-28 21:05:11 -0700

    Add Skill to index. Update imports.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Layout } from "./Layout/Layout";
-export { default as Hero } from "./Hero/Hero";
+export { default as Hero } from "./sections/Hero/Hero";
 export { default as Nav } from "./Nav/Nav";
 export { default as Menu } from "./Menu/Menu";
-export { default as About } from "./About/About";
+export { default as About } from "./sections/About/About";
+export { default as Skill } from "./sections/Skill/Skill";
