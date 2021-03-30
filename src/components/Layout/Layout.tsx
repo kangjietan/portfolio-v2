515c223e7a76a6c1e16610d9ede95c870b9a8905
kangjietan <kangjietan@gmail.com>
2021-03-30 14:11:48 -0700

    Add brand icons to fontawesome library.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import GlobalStyle from "../../themes/GlobalStyle";
 import { Nav } from "../index";
 import { Container } from "./styles";
-// import * as SmoothScroll from "smooth-scroll";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faReact,
+  faNodeJs,
+  faHtml5,
+  faCss3,
+  faJs,
+} from "@fortawesome/free-brands-svg-icons";
+library.add(faReact, faNodeJs, faHtml5, faCss3, faJs);
 
 /**
  * https://medium.com/@chrisfitkin/how-to-smooth-scroll-links-in-gatsby-3dc445299558
  * https://www.gatsbyjs.com/docs/debugging-html-builds/
  */
-const loadSmooth = typeof window !== 'undefined' ? require("smooth-scroll")('a[href*="#"]') : null;
+const loadSmooth =
+  typeof window !== "undefined"
+    ? require("smooth-scroll")('a[href*="#"]')
+    : null;
 
 interface Props {
   children?: any;
