35aefe9a4c764c697e51e77d4c03ced2acab65d6
kangjietan <kangjietan@gmail.com>
2021-03-30 20:29:06 -0700

    Add surface overlay and hover color to skill container.

--- src/components/sections/Skills/Skills.tsx
+++ src/components/sections/Skills/Skills.tsx
@@ -5,6 +5,7 @@ import {
   SectionHeadingContainer,
   SkillContainer,
   SkillName,
+  SkillOverlay,
   SkillsContainer,
   SkillSection,
 } from "./styles";
@@ -18,41 +19,43 @@ import SkillSVG from "./svgs/SkillSVG";
 import Fade from "react-reveal/Fade";
 
 const skillBrands = [
-  { style: "fab", icon: "js", name: "JavaScript" },
-  { style: "fab", icon: "react", name: "React" },
-  { style: "fab", icon: "node-js", name: "Node.js" },
-  { style: "fab", icon: "html5", name: "HTML5" },
-  { style: "fab", icon: "css3-alt", name: "CSS3" },
+  { style: "fab", icon: "js", name: "JavaScript", color: "#f0db4f" },
+  { style: "fab", icon: "react", name: "React", color: "#61DBFB" },
+  { style: "fab", icon: "node-js", name: "Node.js", color: "#68A063" },
+  { style: "fab", icon: "html5", name: "HTML5", color: "#f06529" },
+  { style: "fab", icon: "css3-alt", name: "CSS3", color: "#264de4" },
 ];
 
 const localSkillBrands = [
-  { svg: "typescript", name: "TypeScript" },
-  { svg: "gatsby", name: "Gatsby" },
-  { svg: "redux", name: "Redux" },
+  { svg: "typescript", name: "TypeScript", color: "#007acc" },
+  { svg: "gatsby", name: "Gatsby", color: "#663399" },
+  { svg: "redux", name: "Redux", color: "#764abc" },
 ];
 
 const Skills: React.FunctionComponent = () => {
   const faSkills = skillBrands.map((skill) => {
-    const { style, icon, name } = skill;
+    const { style, icon, name, color } = skill;
     const id = style + icon;
     return (
-      <SkillContainer key={id}>
+      <SkillContainer key={id} color={color}>
         <FontAwesomeIcon
           id={id}
           icon={[style as IconPrefix, icon as IconName]}
           color="var(--on-bg)"
         />
         <SkillName>{name}</SkillName>
+        <SkillOverlay />
       </SkillContainer>
     );
   });
 
   const localSkills = localSkillBrands.map((skill) => {
-    const { svg, name } = skill;
+    const { svg, name, color } = skill;
     return (
-      <SkillContainer key={svg}>
+      <SkillContainer key={svg} color={color}>
         {SkillSVG(svg)}
         <SkillName>{name}</SkillName>
+        <SkillOverlay />
       </SkillContainer>
     );
   });
