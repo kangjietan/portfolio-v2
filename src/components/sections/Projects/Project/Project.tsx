0b402a9132e41793b60f67e7503978ec7b316fac
kangjietan <kangjietan@gmail.com>
2021-04-04 18:26:54 -0700

    Project component.

--- src/components/sections/Projects/Project/Project.tsx
+++ src/components/sections/Projects/Project/Project.tsx
@@ -1,23 +1,59 @@
 import React from "react";
-import { ImageContainer, ProjectContainer } from "./styles";
+import {
+  GitHubLink,
+  ImageContainer,
+  LinksContainer,
+  LiveSiteLink,
+  ProjectContainer,
+  ProjectInformationContainer,
+  ProjectOverlay,
+  ProjectTitle,
+  TechContainer,
+} from "./styles";
 
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { Project as ProjectInterface } from "../Projects";
 
 interface Props {
   project: ProjectInterface;
+  place: number;
 }
 
-const Project: React.FunctionComponent<Props> = ({ project }) => {
+const Project: React.FunctionComponent<Props> = ({ project, place }) => {
   const { title, github, site, tech, image } = project.frontmatter;
   const projectImg = getImage(image)!;
-  console.log(title, github, site, tech, projectImg);
+  console.log(place, title, github, site, tech, projectImg);
+  console.log(place % 2 === 0 ? 2 : 1);
   return (
-    <ProjectContainer>
-      <ImageContainer>
+    <ProjectContainer place={place}>
+      <ImageContainer place={place}>
         <GatsbyImage image={projectImg} alt="project image" />
       </ImageContainer>
+      <ProjectInformationContainer place={place}>
+        <LinksContainer place={place}>
+          <GitHubLink>
+            <FontAwesomeIcon icon={["fab", "github-square"]} size="2x" />
+            <a href={github} target="_blank" rel="noopener noreferrer"></a>
+          </GitHubLink>
+          <LiveSiteLink>
+            <FontAwesomeIcon
+              icon={["fas", "external-link-square-alt"]}
+              size="2x"
+            />
+            <a href={site} target="_blank" rel="noopener noreferrer"></a>
+          </LiveSiteLink>
+        </LinksContainer>
+        <ProjectTitle>{title}</ProjectTitle>
+        <TechContainer>
+          {tech.map((skill) => (
+            <div>{skill}</div>
+          ))}
+        </TechContainer>
+      </ProjectInformationContainer>
+      <ProjectOverlay />
     </ProjectContainer>
   );
 };
--- src/components/sections/Projects/Project/styles.ts
+++ src/components/sections/Projects/Project/styles.ts
@@ -1,10 +1,88 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const ProjectContainer = styled.div``;
+interface ImageContainerProps {
+  place: number;
+}
 
-export const ImageContainer = styled.div`
-  width: 25rem;
-  margin: 1rem;
+interface ProjectContainerProps {
+  place: number;
+}
+
+interface LinksContainerProps {
+  place: number;
+}
+
+interface ProjectInformationContainerProps {
+  place: number;
+}
+
+export const ProjectContainer = styled.div`
+  display: flex;
+  position: relative;
+  color: var(--on-bg);
+  padding: 1rem;
+  margin: 4rem 1rem;
+  justify-content: space-between;
+  max-width: 900px;
+  width: 900px;
+  box-shadow: 0 0 5px var(--primary-color);
+  margin-left: ${(props: ProjectContainerProps) =>
+    props.place % 2 === 0 ? "10rem" : "1rem"};
+  /* transform: ${(props: ProjectContainerProps) =>
+    props.place % 2 === 0 ? "translateX(10rem)" : "translateX(1rem)"}; */
 `;
 
-export const ProjectInformationContainer = styled.div``;
+export const ProjectOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: var(--on-bg);
+  width: 100%;
+  height: 100%;
+  opacity: 7%;
+`;
+
+export const ImageContainer = styled.div`
+  width: 30rem;
+  /* height: 15rem; */
+  transform: translateY(-3rem);
+  /* box-shadow: 0 0 5px var(--primary-color); */
+  order: ${(props: ImageContainerProps) => (props.place % 2 === 0 ? 1 : 0)};
+
+  & > * {
+    box-shadow: 0 0 5px var(--primary-color);
+  }
+`;
+
+export const ProjectInformationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  z-index: 2;
+  text-align: ${(props: ProjectInformationContainerProps) =>
+    props.place % 2 === 0 ? "left" : "right"};
+`;
+
+export const ProjectTitle = styled.div`
+  font-size: 2rem;
+`;
+
+export const LinksContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  justify-content: ${(props: LinksContainerProps) =>
+    props.place % 2 === 0 ? "flex-start" : "flex-end"};
+`;
+
+export const GitHubLink = styled.div`
+  &:hover {
+    color: var(--primary-color);
+  }
+`;
+
+export const LiveSiteLink = styled.div`
+  &:hover {
+    color: var(--primary-color);
+  }
+`;
+
+export const TechContainer = styled.div``;
