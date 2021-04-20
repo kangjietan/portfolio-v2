3d754ea6faa57b1b1aec48a634eeba2ebbf04fd7
kangjietan <kangjietan@gmail.com>
2021-04-19 20:51:22 -0700

    Add index file for hooks.

---
+++ src/components/hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as useOnClickOutside } from "./useOnClickOutside";
+export { default as useScrollDirection } from "./useScrollDirection";
+export { default as useSiteMetadata } from "./useSiteMetadata";
+export { default as useProjectData } from "./useProjectData";
